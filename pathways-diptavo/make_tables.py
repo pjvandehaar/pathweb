ebe4fd842e396b724cfdc5441721166ba300a319
Peter VandeHaar <pjvandehaar@gmail.com>
2019-06-14 16:53:17 -0400

    .

--- pathways-diptavo/make_tables.py
+++ pathways-diptavo/make_tables.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-# phenos.json - [{phecode:'008', best_pval:1e-5, best_pathway:'KEGG_FOO', num_sig_pathways:47}, ...]
-# pathways.json - [{name:'KEGG_FOO', best_pval:1e-5, best_pheno:'008', num_sig_phenos:24}, ...]
+# phenos.json - [{phecode:'008', best_pval:1e-5, best_assoc:'KEGG_FOO', num_sig_assocs:47}, ...]
+# pathways.json - [{name:'KEGG_FOO', best_pval:1e-5, best_assoc:'008', num_sig_assocs:24}, ...]
+
+# TODO: include selected_genes for the best pathways?
+# MAYBE: use sqlite instead of json so that the browser can page?
 
 import sqlite3, json
 conn = sqlite3.connect('pheno_pathway_assoc.db')
@@ -9,10 +12,10 @@ conn.row_factory = sqlite3.Row
 
 pathway_by_id = {}
 for row in conn.execute('SELECT * FROM pathway'):
-    pathway_by_id[row['id']] = dict(name=row['name'], best_pval=999, best_assoc=None, num_sig_assocs=0, id=row['id'])
+    pathway_by_id[row['id']] = dict(name=row['name'], best_pval=999, best_assoc=None, num_sig_assocs=0)
 pheno_by_id = {}
 for row in conn.execute('SELECT * FROM pheno'):
-    pheno_by_id[row['id']] = dict(phecode=row['phecode'], best_pval=999, best_assoc=None, num_sig_assocs=0, id=row['id'])
+    pheno_by_id[row['id']] = dict(phecode=row['phecode'], best_pval=999, best_assoc=None, num_sig_assocs=0)
 
 for i, row in enumerate(conn.execute('SELECT * FROM pheno_pathway_assoc')):
     if i % 1_000_000 == 0: print(i)
@@ -25,5 +28,5 @@ for i, row in enumerate(conn.execute('SELECT * FROM pheno_pathway_assoc')):
         pheno['num_sig_assocs'] += 1
         pathway['num_sig_assocs'] += 1
 
-with open('phenos.json', 'w') as f: json.dump(sorted(pheno_by_id.values(), key=lambda x:x['best_pval']), f)
-with open('pathways.json', 'w') as f: json.dump(sorted(pathway_by_id.values(), key=lambda x:x['best_pval']), f)
+with open('phenos.json', 'w') as f: json.dump(sorted(pheno_by_id.values(), key=lambda x:x['best_pval']), f, separators=(',', ':'))
+with open('pathways.json', 'w') as f: json.dump(sorted(pathway_by_id.values(), key=lambda x:x['best_pval']), f, separators=(',', ':'))
