8c8bd2068056a13c6f50b4eb0477e107a963f2f2
Peter VandeHaar <pjvandehaar@gmail.com>
2019-06-14 23:32:09 -0400

    Add /pathways

---
+++ pathways-diptavo/static/pathways.js
@@ -0,0 +1,20 @@
+'use strict';
+
+$.getJSON('/static/pathways.json').then(function(data) {
+    console.log(data);
+    $(document).ready(function() {
+        var table = new Tabulator('#pathways_table', {
+//            height: 600, // setting height lets Tabulator's VirtualDOM load really fast but makes scrolling awkward
+            layout: 'fitColumns',
+            pagination: 'local',
+            paginationSize: 100,
+            columns: [
+                {title: 'Name', field:'name', formatter:'link', formatterParams: { urlPrefix: '/pathway/' }, widthGrow:5},
+                {title: 'Num p<10<sup>-4</sup> Associations', field:'num_sig_assocs', width:200},
+                {title: 'Best Phenotype', field:'best_assoc'},
+                {title: 'Best P-value', field:'best_pval'},
+            ],
+        });
+        table.setData(data);
+    });
+});
