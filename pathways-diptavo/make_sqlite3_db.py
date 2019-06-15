a9274f1f85d9bb6ff3914c9cc2a3505f1c49c5d5
Peter VandeHaar <pjvandehaar@gmail.com>
2019-06-15 19:36:58 -0400

    Add phenostring and category to db

--- pathways-diptavo/make_sqlite3_db.py
+++ pathways-diptavo/make_sqlite3_db.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import os, re, glob, gzip, sqlite3, itertools
+import os, re, glob, gzip, sqlite3, itertools, csv
 
 # make list of phenotypes: phecode
 filenames = os.listdir('input_data/phenos-2019may')
@@ -14,20 +14,28 @@ for phecode in phecodes:
     for genesettype in genesettypes:
         assert (phecode, genesettype) in phecode_and_genesettype, (phecode, genesettype)
 print(len(phecodes), 'phecodes')
+phenos = {}
+for row in csv.DictReader(open('input_data/Phenotype_Color.csv'), delimiter=','):
+    if row['PheCode'] not in phecodes: continue # un-needed
+    phenos[row['PheCode']] = {
+        'num_cases': int(row['Number.of.cases']),
+        'num_controls': int(row['Number.of.controls']),
+        'num_excluded_controls': int(row['Number.of.excluded.controls']),
+        'phenostring': row['Phenotype.Description'],
+        'category': row['Phenotype.Category'],
+    }
+for phecode in phecodes: assert phecode in phenos, phecode
 
-
-# make list of pathways: name, url, Curated/GO, category, contained_genes
+# make list of pathways: name, url, genesettype(Curated/GO), category, genes
 assert genesettypes == {'GO', 'Curated'} # sanity-check
 pathways = {}
 for genesettype in genesettypes:
     with open('input_data/GMT_files/' + genesettype + '_Subclass.dat') as f:
-        for line in f:
-            name, url, category = line.split()
-            if name == 'NA': continue
-            assert name not in pathways, line
-            pathways[name] = dict(url=url, category=category, genesettype=genesettype)
+        for row in csv.DictReader(f, delimiter=' ', fieldnames=['name','url','category']):
+            if row['name'] == 'NA': continue
+            assert row['name'] not in pathways, row
+            pathways[row['name']] = dict(url=row['url'], category=row['category'], genesettype=genesettype)
 print(len(pathways), 'pathways')
-
 for gmt_filepath in glob.glob('input_data/GMT_files/*.gmt.dat'):
     # note: `.gmt.dat` files appear to just be reformatted versions of the `.gmt` files
     num_matching, num_lines = 0, 0
@@ -60,16 +68,21 @@ assert set(d['category'] for d in pathways.values()) == set('KEGG MOLECULAR BIOC
 # store in sqlite3 as table (pheno, pathway, pval, selected_genes)
 phecode_ids = {phecode: id_ for id_, phecode in enumerate(sorted(phecodes))}
 pathway_ids = {name: id_ for id_, name in enumerate(sorted(pathways.keys()))}
+def pheno_row_generator():
+    for phecode,id_ in phecode_ids.items():
+        p = phenos[phecode]
+        yield (id_, phecode, p['phenostring'], p['category'])
+
 def pathway_row_generator():
     for pathwayname, id_ in pathway_ids.items():
         d = pathways[pathwayname]
         yield (id_, pathwayname, d['url'], d['category'], d['genesettype'], ','.join(d['genes']))
 
 def pheno_pathway_assoc_row_generator(): # note: primary key not included
-    for phecode, genesettype in itertools.product(phecodes, genesettypes):
+    for i, (phecode, genesettype) in enumerate(itertools.product(phecodes, genesettypes)):
         phecode_id = phecode_ids[phecode]
         filename = 'PheCode_{}_{}.wConditional.txt.gz'.format(phecode, genesettype)
-        print(filename)
+        print(i, filename)
         with gzip.open('input_data/phenos-2019may/' + filename, 'rt') as f:
             for line in f:
                 name, url, pval_string, _, selected_genes_string, _ = line.split()
@@ -80,21 +93,22 @@ def pheno_pathway_assoc_row_generator(): # note: primary key not included
                 try:
                     assert name in pathways, line
                     assert pathways[name]['url'] == url, line
-                    assert 0 <= float(pval_string) <= 1, line
+                    assert pval_string == '0' or 1e-6 <= float(pval_string) <= 1, line
                     assert 1 <= len(selected_genes) < 20e3, line
                     assert all(g in pathways[name]['genes'] for g in selected_genes), line
                 except Exception: raise Exception(line)
-                yield (phecode_id, pathway_ids[name], float(pval_string), selected_genes_string)
+                pval = 1e-6 if pval_string=='0' else float(pval_string)
+                yield (phecode_id, pathway_ids[name], pval, selected_genes_string)
 
 db_fname = 'pheno_pathway_assoc.db'
 if os.path.exists(db_fname): raise Exception(db_fname + ' already exists, please delete')
 conn = sqlite3.connect(db_fname)
 with conn: # this commits insertions
-    conn.execute('create table pheno (id INTEGER PRIMARY KEY, phecode VARCHAR)')
+    conn.execute('create table pheno (id INTEGER PRIMARY KEY, phecode VARCHAR, phenostring VARCHAR, category VARCHAR)')
     conn.execute('create table pathway (id INTEGER PRIMARY KEY, name VARCHAR, url VARCHAR, category VARCHAR, genesettype VARCHAR, genes_comma VARCHAR)')
     conn.execute('create table pheno_pathway_assoc (id INTEGER PRIMARY KEY, pheno_id INTEGER, pathway_id INTEGER, pval REAL, selected_genes_comma VARCHAR, FOREIGN KEY(pheno_id) REFERENCES pheno(id), FOREIGN KEY(pathway_id) REFERENCES pathway(id))')
 
-    conn.executemany('INSERT INTO pheno VALUES (?,?)', ((id_, phecode) for phecode,id_ in phecode_ids.items()))
+    conn.executemany('INSERT INTO pheno VALUES (?,?,?,?)', pheno_row_generator())
     conn.executemany('INSERT INTO pathway VALUES (?,?,?,?,?,?)', pathway_row_generator())
     conn.executemany('INSERT INTO pheno_pathway_assoc (pheno_id, pathway_id, pval, selected_genes_comma) VALUES (?,?,?,?)', pheno_pathway_assoc_row_generator())
 
