ad2783196be8038c41b879f7a519caa3da1808c0
Peter VandeHaar <pjvandehaar@gmail.com>
2019-06-16 03:07:50 -0400

    tables: include tooltips for cells with ...

--- pathways-diptavo/static/pheno.js
+++ pathways-diptavo/static/pheno.js
@@ -86,6 +86,22 @@ $.getJSON('/api/pheno/'+model.phecode).then(function(resp) {
             ],
             data: data,
             initialSort: [{column:'pval', dir:'asc'}],
+            tooltipGenerationMode: 'hover', // generate tooltips just-in-time when the data is hovered
+            tooltips: function(cell) {
+                // this function attempts to check whether an ellipsis ('...') is hiding part of the data.
+                // to do so, I compare element.clientWidth against element.scrollWidth;
+                // when scrollWidth is bigger, that means we're hiding part of the data.
+                // unfortunately, the ellipsis sometimes activates too early, meaning that even with clientWidth == scrollWidth some data is hidden by the ellipsis.
+                // fortunately, these tooltips are just a convenience so I don't mind if they fail to show.
+                // I don't know whether clientWidth or offsetWidth is better. clientWidth was more convenient in Chrome74.
+                var e = cell.getElement();
+                //return '' + e.offsetWidth + ' || ' + e.scrollWidth + ' || ' + e.clientWidth;
+                if (e.clientWidth >= e.scrollWidth) {
+                    return false; // all the text is shown, so there is no '...', so no tooltip is needed
+                } else {
+                    return cell.getValue();
+                }
+            }
         });
     });
 });
