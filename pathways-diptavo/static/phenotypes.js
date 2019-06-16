3867d6372a3a50309d511b759229417ad2cd18cb
Peter VandeHaar <pjvandehaar@gmail.com>
2019-06-16 03:33:48 -0400

    Add tooltips to /phenotypes and /pathways

--- pathways-diptavo/static/phenotypes.js
+++ pathways-diptavo/static/phenotypes.js
@@ -15,8 +15,24 @@ $.getJSON('/static/phenotypes.json').then(function(data) {
                 {title: 'Best Pathway', field:'best_assoc', widthGrow:2},
                 {title: 'Best P-value', field:'best_pval'},
             ],
+            tooltipGenerationMode: 'hover', // generate tooltips just-in-time when the data is hovered
+            tooltips: function(cell) {
+                // this function attempts to check whether an ellipsis ('...') is hiding part of the data.
+                // to do so, I compare element.clientWidth against element.scrollWidth;
+                // when scrollWidth is bigger, that means we're hiding part of the data.
+                // unfortunately, the ellipsis sometimes activates too early, meaning that even with clientWidth == scrollWidth some data is hidden by the ellipsis.
+                // fortunately, these tooltips are just a convenience so I don't mind if they fail to show.
+                // I don't know whether clientWidth or offsetWidth is better. clientWidth was more convenient in Chrome74.
+                var e = cell.getElement();
+                //return '' + e.offsetWidth + ' || ' + e.scrollWidth + ' || ' + e.clientWidth;
+                if (e.clientWidth >= e.scrollWidth) {
+                    return false; // all the text is shown, so there is no '...', so no tooltip is needed
+                } else {
+                    return cell.getValue();
+                }
+            },
+            data: data,
+            initialSort: [{column:"num_sig_assocs", dir:"desc"}],
         });
-        table.setData(data);
-        table.setSort([{column:"num_sig_assocs", dir:"desc"}]);
     });
 });
