4691e5c504f1b95e29ca89c60fc8a7e39fb38723
Peter VandeHaar <pjvandehaar@gmail.com>
2019-06-19 18:46:27 -0400

    Rename to `gauss-site`.

--- publish.sh
+++ publish.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$(_readlinkf "${BASH_SOURCE[0]}")")"
 git push
 echo
 
-gcloud compute --project "pheweb" ssh --zone "us-central1-a" "pjvandehaar@phewebish-instance" --command /home/pjvandehaar/shawn-phewebish/pull-and-update.sh
+gcloud compute --project "pheweb" ssh --zone "us-central1-a" "pjvandehaar@phewebish-instance" --command /home/pjvandehaar/gauss-site/pull-and-update.sh
 echo
 
 echo SUCCESSFUL PUBLISH
