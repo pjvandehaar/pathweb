0e7846317e0f7f2f54010d42abdfc5396bf6ef88
Peter VandeHaar <pjvandehaar@gmail.com>
2019-06-19 19:21:19 -0400

    make make_tables.py omit assoc-less phenos and pathways

--- gauss-site/make_tables.py
+++ gauss-site/make_tables.py
@@ -13,25 +13,27 @@ pathway_by_id = {}
 for row in conn.execute('SELECT * FROM pathway'):
     pathway_by_id[row['id']] = dict(
         name=row['name'], category=row['category'],
-        num_sig_assocs=0)
+        num_sig_assocs=0, has_assocs=False)
 pheno_by_id = {}
 for row in conn.execute('SELECT * FROM pheno'):
     pheno_by_id[row['id']] = dict(
         phecode=row['phecode'], phenostring=row['phenostring'], category=row['category'],
         num_cases=row['num_cases'], num_controls=row['num_controls'],
-        num_sig_assocs=0)
+        num_sig_assocs=0, has_assocs=False)
 
 for i, row in enumerate(conn.execute('SELECT * FROM pheno_pathway_assoc')):
     if i % 1_000_000 == 0: print(i)
     pval = row['pval']
     pathway = pathway_by_id[row['pathway_id']]
     pheno = pheno_by_id[row['pheno_id']]
+    pathway['has_assocs'] = pheno['has_assocs'] = True
     if pval < 1e-4:
         pheno['num_sig_assocs'] += 1
         pathway['num_sig_assocs'] += 1
 
+pathway_by_id = {id_:pathway for for id_,pathway in pathway_by_id.items() if not pathway['has_assocs']}
+pheno_by_id = {id_:pheno for for id_,pheno in pheno_by_id.items() if not pheno['has_assocs']}
+
 if not os.path.exists('static'): os.mkdir('static')
-with open('static/phenotypes.json', 'w') as f:
-    json.dump(sorted((p for p in pheno_by_id.values() if p['best_pval'] <= 1), key=lambda x:x['best_pval']), f, separators=(',', ':'))
-with open('static/pathways.json', 'w') as f:
-    json.dump(sorted((p for p in pathway_by_id.values() if p['best_pval'] <= 1), key=lambda x:x['best_pval']), f, separators=(',', ':'))
+with open('static/phenotypes.json', 'w') as f: json.dump(list(pheno_by_id.values()), f, separators=(',', ':'))
+with open('static/pathways.json', 'w') as f: json.dump(list(pathway_by_id.values()), f, separators=(',', ':'))
