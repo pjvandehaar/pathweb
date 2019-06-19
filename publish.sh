a5fed9d0d090519d99461567265b001d08086673
Peter VandeHaar <pjvandehaar@gmail.com>
2019-06-18 22:13:43 -0400

    .

--- publish.sh
+++ publish.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -euo pipefail
+_readlinkf() { perl -MCwd -le 'print Cwd::abs_path shift' "$1"; }
+cd "$(dirname "$(_readlinkf "${BASH_SOURCE[0]}")")"
 
 git push
+echo
+
 gcloud compute --project "pheweb" ssh --zone "us-central1-a" "pjvandehaar@phewebish-instance" --command /home/pjvandehaar/shawn-phewebish/pull-and-update.sh
+echo
 
 echo SUCCESSFUL PUBLISH
--- pull-and-update.sh
+++ pull-and-update.sh
@@ -4,6 +4,9 @@ _readlinkf() { perl -MCwd -le 'print Cwd::abs_path shift' "$1"; }
 cd "$(dirname "$(_readlinkf "${BASH_SOURCE[0]}")")"
 
 git pull --ff-only
+echo
+
 sudo systemctl restart gunicorn-gauss-site
+echo
 
 echo SUCCESSFUL UPDATE
